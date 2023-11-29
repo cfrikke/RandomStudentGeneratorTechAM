e80baa44fd940e3c50645269941af90e3e60ca52
Coco <143611879+cfrikke@users.noreply.github.com>
2023-11-29 13:54:47 +0000

    auto

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             r = rand.Next(0, 39); 
             Console.Clear();
             Console.Write(StudentList.Students[r]);
-            Thread.Sleep(100);
+            Thread.Sleep(1000);
         }
     }
 }
