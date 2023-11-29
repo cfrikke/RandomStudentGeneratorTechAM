e552e69732b4980073240c0c434c72e9603b83c2
Coco <143611879+cfrikke@users.noreply.github.com>
2023-11-29 13:54:06 +0000

    e

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@
         StudentList StudentList = new();
         int r;
         Console.Clear();
-        for(int i = 0; i > 10; i--){
+        for(int i = 0; i > 10; i++){
             r = rand.Next(0, 39); 
             Console.Clear();
             Console.Write(StudentList.Students[r]);
-            Thread.Sleep(1);
+            Thread.Sleep(100);
         }
     }
 }
